47aae02e178e84b511dd10caba93ca4aa76363e0
SabaDzirkvadze12 <sabadirkvadze06@gmail.com>
2024-06-09 14:39:46 +0400

    commit

--- day46/classwork/1.py
+++ day46/classwork/1.py
@@ -1,6 +1,7 @@
-number = int(input("shemoitane ricxvi:  "))
-list1 =[]
-for i in number(1, number + 1):
-    if number % i == 0:
-            list1.append(i)
-print(f"{number}-ის ყველა გამყოფია: {list1}")
+# number = int(input("shemoitane ricxvi:  "))
+# list1 =[]
+# for i in number(1, number + 1):
+#     if number % i == 0:
+#             list1.append(i)
+# print(f"{number}-ის ყველა გამყოფია: {list1}")
+print("zura magaria")
